fb17c5aa50f3e503899eac59c319cb5b50ee2f6c
Alexandra Banica <banica9221@hotmail.be>
2021-03-16 11:23:26 +0100

    detail page

---
+++ View/includes/detail.php
@@ -0,0 +1,45 @@
+
+<section class="general-section justify-content-center">
+
+    <!--<h4>Welcome to the general  page </h4>-->
+    <div class="row">
+        <div class="col-xl-12 pa-0">
+            <div class="search-wrap">
+                <div class="container">
+                    <h4>Look for a specific student via this search bar</h4>
+                    <form action="" method="onsubmit" class="search-form">
+                        <button type="submit" value="search" id="searchButton">search</button>
+                        <input type="text" class="search">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center padding">
+        <div class="table-responsive">
+            <table class="table table-hover" data-toggle="table">
+                <thead>
+                <tr class="justify-content-center">
+                    <th data-sortable="true" scope="col" data-field="Name">Name</th>
+                    <th data-sortable="true" scope="col" data-field="Class">Email</th>
+                    <th data-sortable="true" scope="col" data-field="Class">Class</th>
+                    <th data-sortable="true" scope="col" data-field="Class">Teacher</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr class="clickable" data-href="#">
+                    <td>
+                        Alexandra Banica
+                    </td>
+                    <td>
+                        banica9221@hotmail.be
+                    </td>
+                <td><a class="nav-link" href="general-view.php?page=class">class</a></td>
+                    <td><a class="nav-link" href="general-view.php?page=teacher">Teacher</a></td>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
+
+
