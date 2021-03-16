266eb3dc74dfa069eefbb30ea44a1d7aab055b2b
Alexandra Banica <banica9221@hotmail.be>
2021-03-16 11:21:12 +0100

    if/else statement structure for page reference

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 declare(strict_types=1);
 
 //include all your model files here
-require 'Model/student.php';
+require 'Model/User.php';
 //include all your controllers here
 require 'Controller/HomepageController.php';
 require 'Controller/InfoController.php';
+
+if (!isset($_GET["student"])) {
+
+} elseif ($_GET["teacher"]) {
+
+}
+else {
+
+}
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
