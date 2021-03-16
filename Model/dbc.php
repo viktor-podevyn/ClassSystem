462bfbf7327ea80433c018058868e659acc8b1aa
Mark <mark.hoogkamer@gmail.com>
2021-03-16 15:29:58 +0100

    update

--- Model/dbc.php
+++ Model/dbc.php
@@ -1,7 +1,19 @@
 <?php
+declare(strict_types=1);
 
+function openConnection(): PDO {
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $db = "classsystem";
 
-class dbc
-{
+    $driverOptions = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
 
+    return new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass, $driverOptions);
 }
+
+$pdo = openConnection();
