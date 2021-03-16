8aefd91d2f37ce2d775957193d1a858a67683f47
viktorpodevyn <52883951+viktorpodevyn@users.noreply.github.com>
2021-03-16 15:35:00 +0100

    Loaders replaced

---
+++ Model/class.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types = 1);
+include_once "Entity.php";
+
+class SchoolClass extends Entity
+{
+
+}
---
+++ Model/teacher.php
@@ -0,0 +1,10 @@
+<?php
+
+
+declare(strict_types=1);
+include_once "Entity.php";
+
+class Teacher extends Entity
+{
+
+}
