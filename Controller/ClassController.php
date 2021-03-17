94bf4128356b419197692eee26941759a05d9e0a
Alexandra Banica <banica9221@hotmail.be>
2021-03-17 18:47:49 +0100

    modified classcontr,nothing really changed in studentcontr and teachercontr,made 3 views,modified echo in index

--- Controller/ClassController.php
+++ Controller/ClassController.php
@@ -1,16 +1,16 @@
 <?php
 declare(strict_types = 1);
 
- class ClassController extends ClassLoader
+ class ClassController
 {
     //render function with both $_GET and $_POST vars available if it would be needed.
     public function render(array $GET,array $POST): void
     {
 
-        $class = $this->getClassInfo();
+
 
         //load the view
-        require 'View/general-view.php';
+        require 'View/student-view.php';
         require 'View/detail-view.php';
     }
 }
