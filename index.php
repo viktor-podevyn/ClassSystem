dc33fdb7a2c6bd2837b1ad2c21436aca794e2574
Alexandra Banica <banica9221@hotmail.be>
2021-03-17 01:17:43 +0100

    if/else statement for controller and requires added

--- index.php
+++ index.php
@@ -2,25 +2,32 @@
 declare(strict_types=1);
 
 //include all your model files here
-require 'Model/student.php';
+require 'Model/database.php';
+require 'Model/Student.php';
+require 'Model/ClassLoader.php';
+require 'Model/StudentLoader.php';
+require 'Model/TeacherLoader.php';
+
 //include all your controllers here
-require 'Controller/HomepageController.php';
-require 'Controller/InfoController.php';
-
-if (!isset($_GET["student"])) {
-
-} elseif ($_GET["teacher"]) {
-
-}
-else {
-
-}
+require 'Controller/StudentController.php';
+require 'Controller/TeacherController.php';
+require 'Controller/ClassController.php';
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
 
-$controller = new HomepageController();
-if(isset($_GET['page']) && $_GET['page'] === 'info') {
-    $controller = new InfoController();
+$controller = new StudentController();
+if(isset($_GET['page']) && $_GET['page'] === 'student') {
+    $controller = new StudentController();
+    $controller->render($_GET, $_POST);
 }
-$controller->render($_GET, $_POST);
+
+elseif ($_GET['page'] === 'teacher'){
+    $controller = new TeacherController();
+    $controller->render($_GET, $_POST);
+}
+
+else {
+    $controller = new ClassController();
+    $controller->render($_GET, $_POST);
+}
