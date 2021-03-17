5293cc238327e66bbb44262033520bc02f60ae5a
Alexandra Banica <banica9221@hotmail.be>
2021-03-17 01:18:41 +0100

    studentLoader structure passed to other loaders+other basic phps of classes

---
+++ Model/Student.php
@@ -0,0 +1,24 @@
+<?php
+
+class Student
+{
+    private $name;
+
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getName() : string
+    {
+        return $this->name;
+    }
+}
+
+
+
+
+
+
+
+
--- Model/StudentLoader.php
+++ Model/StudentLoader.php
@@ -1,9 +1,33 @@
-
 <?php
 
-class StudentLoader extends student {
-    public function showUserinfo($name) {
-        $result = $this->getName($name);
-        $connection = $this->openConnection()->query($sql);
+class StudentLoader extends database
+{
+
+    public function getUserInfo(): array
+    {
+        $pdo = $this->openConnection()->prepare('select studentID,concat_ws(" ",firstName,lastName) as name from student');
+        $pdo->execute();
+        return $pdo->fetchAll();
     }
+
+    public function setUserInfo($firstName, $lastName, $email, $className)
+    {
+        $pdo = $this->openConnection()->prepare('insert into student (firstName,lastName,email,className) values (:firstName,:lastName,:email, :className)');
+        $pdo->execute([$firstName, $lastName, $email, $className]);
+    }
+
+    public function edit()
+    {
+        $pdo = $this->openConnection()->prepare('update student set firstName = :firstName, lastName = :lastName,email = :email, className = :className where studentID = :id');
+        $pdo->execute();
+    }
+
+    public function delete()
+    {
+        $pdo = $this->openConnection()->prepare('delete from student where studentID = :id');
+        $pdo->execute();
+    }
+
+
+
 }
