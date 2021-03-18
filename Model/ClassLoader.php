9273d463e6e1bdc5ba9af5ea4fade124151a9b73
viktor-podevyn <76952208+viktor-podevyn@users.noreply.github.com>
2021-03-18 15:19:54 +0100

    Update ClassLoader.php

--- Model/ClassLoader.php
+++ Model/ClassLoader.php
@@ -1,16 +1,46 @@
 <?php
 
-class ClassLoader extends database {
+class ClassLoader extends database
+{
 
-    public function getClassInfo(): array
+    public function getClassInfo($id): array
     {
-        $pdo = $this->openConnection()->prepare('select location as classLocation from class');
+        $pdo = $this->openConnection()->prepare('select className, class.classID from class left join class on class.classID = class.teacherID where class.classID = :id');
+        $pdo->bindValue(':id', $id);
         $pdo->execute();
         return $pdo->fetchAll();
     }
 
-    public function setClassInfo($className,$location,$studentID,$teacherID) {
-        $pdo = $this->openConnection()->prepare('insert into class ($className,$location,$studentID,$teacherID) values (:className,:$location,:$studentID,:$teacherID)');
-        $pdo->execute([$className,$location,$studentID,$teacherID]);
+    public function getClassesInfo(): array
+    {
+        $pdo = $this->openConnection();
+        $handle = $pdo->prepare('select className, class.classID, location from class');
+        $handle->execute();
+        return $handle->fetchAll();
+
     }
+
+    public function setClassesInfo($classID, $className, $location, $teacherID)
+    {
+        $pdo = $this->openConnection()->prepare('insert into class (classID, className, location, teacherID) values (:classID,:className,:location, :teacherID)');
+        $pdo->execute([$classID, $className, $location, $teacherID]);
+    }
+
+    public function edit($id)
+    {
+        $pdo = $this->openConnection();
+        $handle = $pdo->prepare('update class set firstName = :firstName, lastName = :lastName,email = :email, className = :className where teacherID = :id');
+        $handle->bindValue(':id', $id);
+        $handle->execute();
+    }
+
+    public function delete($id): void
+    {
+        $pdo = $this->openConnection();
+        $handle = $pdo->prepare('delete from class where classID = :id');
+        $handle->bindValue(':id', $id);
+        $handle->execute();
+    }
+
+
 }
