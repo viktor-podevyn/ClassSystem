f6bf93256e9e4437b33109a9cd684d63be0e49c0
viktor-podevyn <76952208+viktor-podevyn@users.noreply.github.com>
2021-03-18 15:21:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,18 +15,19 @@ require 'Controller/ClassController.php';
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
+if (isset($_GET['page'])){
+    if($_GET['page'] === 'student') {
+        $controller = new StudentController();
+        $controller->render($_GET, $_POST);
+    }
 
- if(isset($_GET['page']) && $_GET['page'] === 'student') {
-    $controller = new StudentController();
-    $controller->render($_GET, $_POST);
-}
-
-elseif ($_GET['page']??NULL === 'teacher'){
-    $controller = new TeacherController();
-    $controller->render($_GET, $_POST);
-}
-else {
-    $controller = new ClassController();
-    $controller->render($_GET, $_POST);
+    elseif ($_GET['page'] === 'teacher'){
+        $controller = new TeacherController();
+        $controller->render($_GET, $_POST);
+    }
+    else {
+        $controller = new ClassController();
+        $controller->render($_GET, $_POST);
+    }
 }
 
