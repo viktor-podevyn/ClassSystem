f8165419ef43ca5d71679d3e7a70a360d0836c7d
Mark <mark.hoogkamer@gmail.com>
2021-03-18 13:42:11 +0100

    Updated search php and edited generalstudents

--- Model/search.php
+++ Model/search.php
@@ -4,35 +4,33 @@ $con = new PDO("mysql:host=localhost;dbname=crud",'root','');
 
 if (isset($_POST["submit"])) {
     $str = $_POST["search"];
-    $sth = $con->prepare("SELECT * FROM student WHERE Name = '$str'");
+    $sth = $con->prepare("SELECT * FROM student WHERE firstName = '$str'");
 
     $sth->setFetchMode(PDO:: FETCH_OBJ);
     $sth -> execute();
 
-    if($row = $sth->fetch())
-    {
+    if($row = $sth->fetch()) {
         ?>
         <br><br><br>
         <table>
             <tr>
                 <th>Name</th>
-                <th>Description</th>
+                <th>Last name</th>
+                <th>Student ID</th>
+                <th>Class Name</th>
             </tr>
             <tr>
-                <td><?php echo $row->Name; ?></td>
-                <td><?php echo $row->Description;?></td>
+                <td><?php echo $row->firstName; ?></td>
+                <td><?php echo $row->lastName;?></td>
+                <td><?php echo $row->studentID;?></td>
+                <td><?php echo $row->className;?></td>
             </tr>
 
         </table>
         <?php
-    }
-
-
-    else{
+    } else{
         echo "Name Does not exist";
     }
-
-
 }
 
 ?>
